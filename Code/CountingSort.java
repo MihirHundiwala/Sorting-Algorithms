371ca3fc7bf9aa2f72c8b9d89af6607662444e31
MihirHundiwala <mihir.hundiwala@somaiya.edu>
2022-06-25 00:06:23 +0530

    Added Comments

--- Code/CountingSort.java
+++ Code/CountingSort.java
@@ -1,7 +1,14 @@
 import java.util.Arrays;
 public class CountingSort {
     
-    /*
+    /*  
+        -> We count the occurences of each element and store it in an array
+        -> Find cumulative sum of the array in order,
+           to get the indices of elements in output array (this is 1-based indexing).
+        -> Use cumulative sum to place elements in output array
+           Note that here we subtract 1 to convert the above to 0-based indexing.
+
+
         -> Time Complexity : O(n)
         -> Auxiliary Space Complexity : O(n)
         -> Inplace? Indirectly Yes
@@ -14,15 +21,21 @@ public class CountingSort {
 
         int max = Arrays.stream(arr).max().getAsInt();
         int min = Arrays.stream(arr).min().getAsInt();
+
+        // Number of elements between min and max including both
         int range = max - min + 1;
 
         int count[] = new int[range];
         int output[] = new int[n];
 
+        // min is used as an offset
+
+        // Increase count
         for (int i = 0; i < n; i++) {
             count[arr[i] - min]++;
         }
   
+        // Cumulative sum
         for (int i = 1; i < count.length; i++) {
             count[i] += count[i - 1];
         }
